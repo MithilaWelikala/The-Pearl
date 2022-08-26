4cc5f043b44852f6897e1eb43f5b3e8181ad977d
Mithila Welikala <68138141+MithilaWelikala@users.noreply.github.com>
2022-08-27 01:37:50 +0530

    Add files via upload

---
+++ scripts/indexjavascript.js
@@ -0,0 +1,25 @@
+// open after 1 seconds
+setTimeout(() => window.open('/index.html'), 1000);
+function myFunction() {
+  alert("Hello! I am an alert box!");
+}
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
